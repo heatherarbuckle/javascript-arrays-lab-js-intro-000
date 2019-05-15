2e1390fc27395d9eddf82b554e0b7e0542f3d9d3
Heather Arbuckle <bucklehead@gmail.com>
2019-05-15 18:06:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function appendKitten(name) {
 
 function prependKitten(name) {
   const newKitten = [name, ...kittens];
-  return addKittens;
+  return newKittens;
 }
 
 function removeLastKitten() {
